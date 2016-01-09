b2817872c53cf55384eaede063e9c71d5cf01ecb
Yanmin <ymxing@thoughtworks.com>
2016-01-09 23:08:47 +0800

    [R-R] Yanmin:remove no use code.

--- src/js/components/blogs.jsx
+++ src/js/components/blogs.jsx
@@ -5,8 +5,8 @@ module.exports = React.createClass({
     render: function(){
         return (
             <div>
-                <li><a href="#/blog/yanmin">Yanmin's blogs</a></li>
-                <li><a href="#/blog/xxx">xxx's blogs</a></li>
+                <li><a href="/blogs/yanmin">Yanmin's blogs</a></li>
+                <li><a href="/blogs/xxx">xxx's blogs</a></li>
             </div>
         )
     }
